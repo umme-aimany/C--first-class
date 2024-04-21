029a22271735abaa21a846efe2af24a007b66395
Umme Aiman <122363026+umme-aimany@users.noreply.github.com>
2024-04-22 01:38:11 +0500

    Add files via upload

---
+++ aiman/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+  main () 
+{
+//     cout<<"Aiman khan";
+   
+//     cout<<" today is friday   ";
+//     ////////////VARIABLES DATA TYPE ////////////
+//    int x;
+//    int y;
+//     x=10;
+//     y=5;
+//   cout<< x + y << endl;
+// ///////////////////////////////////////////////////////////////////////
+//       int age;
+//       cout<<"enter your age ";
+//       cin >> age;
+//       cout<< age ;
+// ////////////////store multiples ages and see output///////////////////////
+//  int age1,age2,age3;
+//  cout<<"Enter your first age ";
+//  cin >>age1;
+//  cout<<"enter your secong age ";
+//  cin >>age2;
+//  cout<<"Enter yopur third age ";
+//  cin>>age3;
+//  int total;
+//  total= age1+age2+age3;
+//  cout<<"total ages are:" <<total <<endl;
+//  int average;
+//  average=total/3;
+//  cout<<average;
+//////////////////////////IF & else statementr///////////////////
+  if (20==10)
+  {
+   cout<<" true value";
+  }
+  else{
+    cout<<"your condiotion is false  ";
+  }
+  int age1,age2;
+  age1=10;
+  age2=20;
+  if(age1>age2){
+    cout<<"  you are eligible";
+
+    }
+    else
+{
+      cout<<"   you are not eligible";
+  }
+  int age3,age4;
+  cout<<"Enter your age3";
+  cin >>age3;
+  cout<<"Enter your age 4";
+  cin >> age4;
+  if (age3!=age4){
+    cout<<"Your are young";
+  }
+  else{
+    cout<<"you are old";
+  }
+  
+  ///////////////////////conditional operators////////////////
+   string username;
+   cout<<"Enter your name";
+   cin >>username;
+   int password=12345;
+   cout<<"Enter your password";
+   cin >>password;
+   cout<< "username" "password";
+   if(username=="Abdullah"&& password==123)
+   {
+    cout<<"YOU ARE SUCCESFULLY LOGIN  ";
+
+   }
+   else{
+    cout<<"  please inter correct info";
+   }
+////////////////////////LOOP/////////////////
+//////////////////WHILE LOOP//////////////////////
+int x=1;
+while (x<=5)
+{
+ cout<<"YOUR ARE MY BEST FRIEND HALEEMA";
+ x++;
+ 
+
+
+
+}
